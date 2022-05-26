916dc09a4660e8a04e2d2436e69f4b7c7b9da2bf
Milica Urukalo <74359178+milica38@users.noreply.github.com>
2022-05-26 16:23:11 +0200

    Registration logic

--- agent-app/src/app/registration/registration.component.ts
+++ agent-app/src/app/registration/registration.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +11,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;  
+  hide = true;
+
+  constructor(
+    private formBuilder : FormBuilder,
+    private router: Router,
+    private api: ApiService,
+    private _snackBar: MatSnackBar
+  ) { 
+    this.form = this.formBuilder.group({
+      name: ['', Validators.compose([Validators.required, Validators.pattern('[a-zčćžšđA-ZČĆŽŠĐ]*')])],
+      surname: ['', Validators.compose([Validators.required, Validators.pattern('[a-zčćžšđA-ZČĆŽŠĐ]*')])],
+      email: ['', Validators.email],
+      username: ['', Validators.required ],
+      password: ['', Validators.required],
+      passwordRepeat: ['', Validators.required],
+      phoneNumber: ['', Validators.compose([Validators.required, Validators.minLength(10)])],
+      gender: ['', Validators.required],
+      birthDate: ['', Validators.required]
+    })
+  }
 
   ngOnInit(): void {
   }
+
+  onSubmit() {
+    if(this.form.valid) {
+
+      const name = this.form.get('name')?.value;
+      const surname = this.form.get('surname')?.value;
+      const email = this.form.get('email')?.value;
+      const password = this.form.get('password')?.value;
+      const passwordRepeat = this.form.get('passwordRepeat')?.value;
+      const username = this.form.get('username')?.value;
+      const phoneNumber = this.form.get('phoneNumber')?.value;
+      const gender = this.form.get('gender')?.value;
+      const birthDate = this.form.get('birthDate')?.value;
+
+      if(passwordRepeat != password) {
+        alert('Passwords do not match')
+        return;
+      }
+
+      let data = {
+        name: name,
+        surname: surname,
+        email: email,
+        username: username,
+        password: password,
+        phoneNumber: phoneNumber,
+        gender: gender,
+        birthDate: birthDate
+      }
+
+      this.api.registerUser(data).subscribe((response: any) => {
+        console.log(response)
+    });
+
+  }
+}
 
 }
