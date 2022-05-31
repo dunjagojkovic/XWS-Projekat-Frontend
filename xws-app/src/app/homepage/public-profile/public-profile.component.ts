90374c397b8bda663743bf64d20001f0464e57df
Milica Urukalo <74359178+milica38@users.noreply.github.com>
2022-05-31 23:12:00 +0200

    Show posts on public profile

--- xws-app/src/app/homepage/public-profile/public-profile.component.ts
+++ xws-app/src/app/homepage/public-profile/public-profile.component.ts
@@ -18,18 +18,34 @@ export class PublicProfileComponent implements OnInit {
   username: any;
   userPosts: any = {} as any
   public posts: any[]
+  name: any;
+  surname: any;
+
+  public commentList: any[]
+  public likeList: any[]
+  public dislikeList: any[]
+  postId = ""
+  backPost = ""
+  likeBack = ""
+  dislikeBack = ""
+  postLikeId = ""
+  postDislikeId = ""
+  userComment: any = {} as any
+  userLike: any = {} as any
+  userDislike: any = {} as any
+
 
   constructor(
     private formBuilder : FormBuilder,
     private router: Router,
-    private route: ActivatedRoute,
+    public route: ActivatedRoute,
     private api: ApiService,   
     private service: PostServiceService
   ) { 
     
     this.route.queryParams.subscribe(params => {
-      let username = params['username'];
-      console.log(username); // Print the parameter to the console. 
+      this.username = params['username'];
+      console.log(this.username); // Print the parameter to the console. 
   })
 
     this.form = this.formBuilder.group({
@@ -46,11 +62,56 @@ export class PublicProfileComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.service.userPosts(this.user.username).subscribe((response: any) => {
+    this.service.userPosts(this.username).subscribe((response: any) => {
       this.userPosts = response;
       this.posts = this.userPosts.posts
       console.log(this.posts);
     });
+  }
+
+  comments(id: string) {
+    this.service.postComments(id).subscribe((response: any) => {
+        this.userComment = response;
+        this.commentList = this.userComment.comments
+    })   
+  
+    this.postId = id
+    this.backPost = ""
+  }
+
+  back(id: string) {
+    this.backPost = id;
+    this.postId = "";
+  }
+
+  likes(id: string) {
+
+    this.service.postLikes(id).subscribe((response: any) => {
+      this.userLike = response
+      this.likeList = response.likes
+    })
+    this.postLikeId = id
+    this.likeBack = "";
+  }
+
+  dislikes(id: string) {
+    this.service.postDislikes(id).subscribe((response: any) => {
+      this.userDislike = response
+      this.dislikeList = this.userDislike.likes
+    })
+    this.postDislikeId = id
+    this.dislikeBack = "";
+
+  }
+
+  backLike(id: string) {
+    this.likeBack = id
+    this.postLikeId = ""
+  }
+
+  backDislike(id: string) {
+    this.dislikeBack = id
+    this.postDislikeId = ""
   }
 
 }
