e0a63ae21c7e95603bc703e4f77a059a135e7bbe
Dunja Gojković <74664594+dunjagojkovic@users.noreply.github.com>
2022-06-28 20:49:22 +0200

    fix port for agent

--- agent-app/src/app/api.service.ts
+++ agent-app/src/app/api.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
 
 
-  baseURL = "http://localhost:8083";
+  baseURL = "http://localhost:8080";
   baseURL2 = "http://localhost:8000";
 
   constructor(private http: HttpClient) { }
